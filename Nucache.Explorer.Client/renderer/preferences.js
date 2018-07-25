28d7d033cc914a7e8819f4fa47a2f91aa614b104
Warren <warren@umbraco.com>
2018-07-25 14:41:58 +0100

    Adds in electron store. Reads value & uses a default value fallback if no config file on disk yet or value cannot be read
    Sets the theme everytime we change the dropdown before we send out IPC message that Vue uses to update CodeMirror

--- Nucache.Explorer.Client/renderer/preferences.js
+++ Nucache.Explorer.Client/renderer/preferences.js
@@ -1,6 +1,9 @@
 const { ipcRenderer } = require('electron'); //Send message back to main.js & app-menu.js that can do the Web API call
 const log = require('electron-log');
 
+const Store = require('electron-store');
+const store = new Store();
+
 document.addEventListener('DOMContentLoaded', function() {
 
     //Select the element in the DOM that is the dropdown
@@ -9,8 +12,10 @@ document.addEventListener('DOMContentLoaded', function() {
     //Bind the dropdown element to run a function when the onchange event is emitted
     themeSelector.onchange = changeTheme;
 
-    //Set selected theme from user stored preference
-    themeSelector.value = "neat";
+    //Set selected theme from user stored preference from key 'theme' if nothing set then
+    //falback to our default theme monokai
+    var selectedTheme = store.get('theme', 'neat');
+    themeSelector.value = selectedTheme;
 
 });
 
@@ -19,8 +24,8 @@ function changeTheme(event) {
     if(event.target.value){
         log.info(`Theme preference changed to - ${event.target.value}`);
 
-        //Save/persist the change to pref storage
-
+        //Save/persist the change to pref storage with key 'theme'
+        store.set('theme', event.target.value);
         
         //Send message back to main IPC - which will then proxy/send it back out to the renderer
         //Where we can listen for it in the parent/main window & update the vue js

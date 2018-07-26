7d64271aa4e7bf817fbd9c5d7f83f3494eaaa797
Warren <warren@umbraco.com>
2018-07-26 11:37:46 +0100

    Dynamically load all CodeMirror CSS themes that it ships with to the dropdown/select

--- Nucache.Explorer.Client/renderer/preferences.js
+++ Nucache.Explorer.Client/renderer/preferences.js
@@ -1,5 +1,7 @@
 const { ipcRenderer } = require('electron'); //Send message back to main.js & app-menu.js that can do the Web API call
 const log = require('electron-log');
+const fs = require('fs');
+const path = require('path');
 
 const Store = require('electron-store');
 const store = new Store();
@@ -9,13 +11,31 @@ document.addEventListener('DOMContentLoaded', function() {
     //Select the element in the DOM that is the dropdown
     var themeSelector = document.getElementById('theme-selector');
 
+
+    //Populate dropdown list with themes from node_modules/codemirror/theme/
+    const directoryPath = path.join(__dirname, '/node_modules/codemirror/theme');
+
+    fs.readdir(directoryPath, function (err, files) {
+        //handling error
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+        //listing all files using forEach
+        files.forEach(function (file) {
+
+            var option = document.createElement('option');
+            option.text = file.replace('.css', '');
+            themeSelector.add(option);
+        });
+
+         //Set selected theme from user stored preference from key 'theme' if nothing set then
+        //falback to our default theme monokai
+        var selectedTheme = store.get('theme', 'monokai');
+        themeSelector.value = selectedTheme;
+    });
+
     //Bind the dropdown element to run a function when the onchange event is emitted
     themeSelector.onchange = changeTheme;
-
-    //Set selected theme from user stored preference from key 'theme' if nothing set then
-    //falback to our default theme monokai
-    var selectedTheme = store.get('theme', 'monokai');
-    themeSelector.value = selectedTheme;
 
 });
 
